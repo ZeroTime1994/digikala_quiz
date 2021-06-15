a4b20ab5aca53291af8709877c0eb0f886fd67a6
Alireza <alireza.zero.time1994@gmail.com>
2021-06-15 18:20:08 +0430

    add product card and show products in products page

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="product-card">
+    <img :src="imageUrl" :alt="title" />
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "ProductCard",
+  props: {
+    title: {
+      type: String as PropType<string>,
+      requried: true,
+    },
+    imageUrl: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="scss" scoped></style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>Products</div>
+  <div>
+    <template v-if="products">
+      <product-card
+        v-for="(product, index) in products"
+        :key="index"
+        :title="product.title"
+        :imageUrl="product.images.main"
+      />
+    </template>
+  </div>
 </template>
 <script lang="ts">
+//Composition Api
 import { computed, defineComponent, onMounted } from "@vue/composition-api";
+
+//Interfaces And Types
 import { RootState } from "@/store";
 import { ProductActionTypes } from "@/store/product/action-types";
 
+//Components
+import ProductCard from "@/components/ProductCard.vue";
+
 export default defineComponent({
+  components: { ProductCard },
   setup(_, { root }) {
     const store = root.$store;
     const state = store.state as RootState;
