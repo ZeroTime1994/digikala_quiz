ffff32d043bcfa7d46a9a7e976a833e41d3beb1b
Alireza <alireza.zero.time1994@gmail.com>
2021-06-15 15:24:52 +0430

    clean up and add composition api and create php page

---
+++ src/views/Products.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Products</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const products: string[] = [];
+
+    return {
+      products,
+    };
+  },
+});
+</script>
