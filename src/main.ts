2282147c39dba29ecd5c6b369670fb8e1a4a5e3a
Alireza <alireza.zero.time1994@gmail.com>
2021-06-16 16:45:56 +0430

    add material icons

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from "./store";
 import "./plugins/axios";
 import VueCompositionApi from "@vue/composition-api";
 import "./assets/scss/main.scss";
+import "@mdi/font/css/materialdesignicons.min.css";
 
 Vue.config.productionTip = false;
 
